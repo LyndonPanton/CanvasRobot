ec76ee94b700be3c5028af1f56eeddc2fa677dbe
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-20 21:47:41 +0100

    Grabbed the canvas, context, defined function for drawing shapes

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function() {
+	const canvas = document.getElementsByTagName("canvas")[0];
+	let context = canvas.getContext("2d");
+
+	function drawPolygon(xStart, yStart, xLength, yLength, rotation, colour) {
+		context.save();
+
+		
+
+		context.restore();
+	}
+}());
