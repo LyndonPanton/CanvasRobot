ba01ce2e75e65d61ec9c826ff3ecd3bf6310317f
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-20 22:06:54 +0100

    Defined dimensions of the canvas, drew robot mouth

--- js/app.js
+++ js/app.js
@@ -1,11 +1,37 @@
 (function() {
 	const canvas = document.getElementsByTagName("canvas")[0];
+	canvas.width = window.innerWidth - 100;
+	canvas.height = window.innerHeight - 100;
 	let context = canvas.getContext("2d");
+
+	// Head
+	drawPolygon(400, 0, 100, 100, 0, "olive");
+
+	// Eye Left
+	drawPolygon(410, 10, 15, 15, 0, "white");
+
+	// Pupil Left
+	drawPolygon(413, 13, 8, 8, 0, "black");
+
+	// Eye Right
+	drawPolygon(475, 10, 15, 15, 0, "white");
+
+	// Pupil Right
+	drawPolygon(478, 13, 8, 8, 0, "black");
+
+	// Mouth
+	drawPolygon(420, 60, 60, 20, "black");
 
 	function drawPolygon(xStart, yStart, xLength, yLength, rotation, colour) {
 		context.save();
 
-		
+		context.fillStyle = colour;
+
+		if (rotation) {
+			context.rotate(Math.PI / 180 * rotation);
+		}
+
+		context.fillRect(xStart, yStart, xLength, yLength);
 
 		context.restore();
 	}
