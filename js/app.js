968a092bcf59e50cb975004c4d62d554f1096fcd
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-20 23:02:05 +0100

    Drew robot shoulders, arms, hands, thumbs

--- js/app.js
+++ js/app.js
@@ -19,13 +19,10 @@
 
 	// Neck
 	drawPolygon(420, 100, 60, 30, 0, "brown");
-
 	// Chest
 	drawPolygon(340, 130, 220, 130, 0, "teal");
-
 	// Stomach
 	drawPolygon(380, 260, 140, 80, 0, "green");
-
 	// Waist
 	context.fillStyle = "green";
 	context.moveTo(350, 340);
@@ -38,15 +35,39 @@
 
 	// Leg Left
 	drawPolygon(440, 290, 90, 180, 15, "orange");
-
 	// Leg right
 	drawPolygon(340, 525, 90, 180, 345, "orange");
-
 	// Foot Left
 	drawPolygon(304, 560, 95, 50, 0, "brown");
-
 	// Foot Right
 	drawPolygon(503, 560, 95, 50, 0, "brown");
+
+	// Left Shoulder
+	drawPolygon(280, 130, 60, 45, 0, "brown");
+
+	// Left Arm
+	drawPolygon(280, 175, 50, 110, 0, "purple");
+
+	// Left Hand
+	drawPolygon(180, 365, 20, 50, 340, "olive");
+
+	// Left Thumb
+	drawPolygon(50, 410, 15, 30, 320, "olive");
+
+
+	// Right Shoulder
+	drawPolygon(560, 130, 60, 45, 0, "brown");
+
+	// Right Arm
+	drawPolygon(570, 175, 50, 110, 0, "purple");
+
+	// Right Hand
+	drawPolygon(640, 110, 20, 50, 15, "olive");
+
+	// Right Thumb
+	drawPolygon(640, -120, 15, 30, 35, "olive");
+
+
 
 	function drawPolygon(xStart, yStart, xLength, yLength, rotation, colour) {
 		context.save();
